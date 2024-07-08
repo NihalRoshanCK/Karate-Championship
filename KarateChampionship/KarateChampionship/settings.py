088142259866ee9c679a27cea3ac63cd2254ee32
Nihal Roshan <82870227+Nihalroshan55@users.noreply.github.com>
2024-07-08 18:46:21 +0000

    cors updated

--- KarateChampionship/KarateChampionship/settings.py
+++ KarateChampionship/KarateChampionship/settings.py
@@ -199,5 +199,6 @@ CORS_ALLOWED_ORIGINS = [
     "http://localhost:5173",
     "https://takeshi-mu.vercel.app",
     "https://takeshi-qr6kpr1kx-nihalroshan55s-projects.vercel.app",
-    "https://kenyuryu-championship-client-git-6fed92-nihalroshan55s-projects.vercel.app"
+    "https://kenyuryu-championship-client-git-6fed92-nihalroshan55s-projects.vercel.app",
+    "https://kenyuryu.vercel.app"
 ]
